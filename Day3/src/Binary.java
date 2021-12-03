9eb8409810d45a263a45b6e01fa129f95116dd1b
Viktor Nemeth <viand21@student.sdu.dk>
2021-12-03 11:42:13 +0100

    REKURSIV LØSNING_ Day 3_part 2

--- Day3/src/Binary.java
+++ Day3/src/Binary.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Binary {
@@ -10,6 +9,62 @@ public class Binary {
         System.out.println(Gammarate(myList));
         //Decimal number = 199
         //Decimal number 2 = 3896
+
+        System.out.println(rating(myList,0,"O"));
+        //509
+        System.out.println(rating(myList,0,"CO2"));
+        //2693
+
+
+
+    }
+
+    public static String oxygen_generator_rating(ArrayList<String> list){
+        String finalString = "";
+
+        return finalString;
+    }
+
+    public static boolean most_of(ArrayList<String> list, int index,String rating){
+        int most_of = 0;
+
+        for (String string:list) {
+            char c = string.charAt(index);
+            if(c=='1') most_of += 1;
+            else if(c=='0') most_of -=1;
+        }
+
+        //true = most ones
+        //false = most zeros
+        if(rating.equals("O"))
+            return most_of>=0;
+        if(rating.equals("CO2"))
+            return most_of < 0;
+        else{
+            System.out.println("Wrong rating input");
+            return false;
+        }
+    }
+
+    public static ArrayList<String> rating(ArrayList<String> list, int index, String method){
+        if(list.size()==1) return list;
+
+        ArrayList<String> newList = new ArrayList<String>();
+
+        char bit_criteria = (most_of(list,index,method)) ? '1' : '0';
+
+        for (String string:list) {
+            if(bit_criteria == string.charAt(index)){
+                newList.add(string);
+            }
+        }
+
+        if(index == 11 && list.size()>1){
+            index = 0;
+        }
+
+
+        return rating(newList,index+1,method);
     }
 
 
